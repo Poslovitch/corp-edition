53e4c6e96617feb07029238fbdb75fc83540c421
Florian CUNY <poslovitch@bentobox.world>
2020-04-16 01:35:02 +0200

    Added in-editor description for Teleport module

--- @corp_edition/addons/corp_edition_teleport/config.cpp
+++ @corp_edition/addons/corp_edition_teleport/config.cpp
@@ -1,45 +1,59 @@
 class CfgPatches {
-    class CORP_Edition_Teleport {
-        units[] = {"CORP_Module_Teleport"};
-        author = "CORP Modding Studio";
-        requiredVersion = 1.98;
-        requiredAddons[] = {"A3_Modules_F", "corp_edition_core"};
-    };
+  class CORP_Edition_Teleport {
+    units[] = {"CORP_Module_Teleport"};
+    author = "CORP Modding Studio";
+    requiredVersion = 1.98;
+    requiredAddons[] = {"A3_Modules_F", "corp_edition_core"};
+  };
 };
 
 class CfgFunctions {
-    class CORP {
-        tag = "CORP";
+  class CORP {
+    tag = "CORP";
 
-        class CORPEditionTeleport {
-            file = "\corp_edition_teleport\functions";
-            class Teleport_Init {};
-            class Teleport_UiStart {};
-        };
+    class CORPEditionTeleport {
+      file = "\corp_edition_teleport\functions";
+      class Teleport_Init {};
+      class Teleport_UiStart {};
     };
+  };
 };
 
 class CfgVehicles {
-    class Logic;
-    class Module_F: Logic {
-        class ModuleDescription {
-            class AnyStaticObject;
-        };
+  class Logic;
+  class Module_F: Logic {
+    class AttributesBase {
+      class ModuleDescription;
+    };
+    class ModuleDescription
+    {
+      class AnyStaticObject;
+    };
+  };
+
+  class CORP_Module_Teleport: Module_F {
+    scope = 2;
+    displayName = $STR_CORP_TELEPORT_DN;
+    icon = "\corp_edition_teleport\icon.paa";
+    category = "CORP_Modules";
+
+    function = "CORP_fnc_teleport_init";
+    functionPriority = 1;
+    isGlobal = 1;
+    isTriggerActivated = 0;
+    isDisposable = 0;
+    is3DEN = 0;
+
+    class Attributes: AttributesBase {
+      class ModuleDescription: ModuleDescription{};
     };
 
-    class CORP_Module_Teleport: Module_F {
-        scope = 2;
-        displayName = $STR_CORP_TELEPORT_DN;
-        icon = "\corp_edition_teleport\icon.paa";
-        category = "CORP_Modules";
-
-        function = "CORP_fnc_teleport_init";
-        functionPriority = 1;
-        isGlobal = 1;
-        isTriggerActivated = 0;
-        isDisposable = 0;
-        is3DEN = 0;
+    class ModuleDescription: ModuleDescription {
+      description = $STR_CORP_TELEPORT_DESC;
+      sync[] = {"AnyStaticObject"};
+      duplicate = 0;
     };
+  };
 };
 
 #include "ui\ui.hpp"
